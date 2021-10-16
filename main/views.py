437b5a59d82aa562848dc97c1cadce4ab12a06d9
Christopher Ekaputra Loe <christopher.ekaputra@ui.ac.id>
2021-10-16 13:52:49 +0700

    Home Page - Chris

--- main/views.py
+++ main/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
-
+import requests
 
 def home(request):
-    return render(request, 'home.html')
+    response = requests.get('https://api.kawalcorona.com/indonesia')
+    coronaData = response.json()
+    return render(request, 'home.html', {
+        'name': coronaData[0]['name'],
+        'positif': coronaData[0]['positif'],
+        'sembuh': coronaData[0]['sembuh'],
+        'meninggal': coronaData[0]['meninggal'],
+    })
