cfc9fa42acdd72006352ff05d840d6af09a3b681
Alya Azhar Agharid <alya.azhar@ui.ac.id>
2021-10-22 14:22:38 +0700

    Create Forum Form

--- forum/views.py
+++ forum/views.py
@@ -1,11 +1,9 @@
-from typing import NewType
 from django.http import response
 from django.shortcuts import render,redirect
-from .models import PostForum
 from django.views import View
+from .models import PostForum
 from .forms import InputForum
 
-name = "asajsjaaaskeee"
 def forum(request): # ini buat nulis isi boxnya apa aja
     PostContent = PostForum.objects.all().values()
     response = {'PostContent': PostContent}
@@ -14,10 +12,10 @@ def forum(request): # ini buat nulis isi boxnya apa aja
 # ini buat formnya
 def add_post(request):
     inputPost = InputForum
-    response = {'name':name,'inputContent':inputPost}
+    response = {'inputContent':inputPost}
     if request.method == 'POST':
         post = InputForum(request.POST)
         if post.is_valid():
             post.save()
-            return redirect('forum/')
+            return redirect('../forum')
     return render(request, 'add_forum.html',response)
