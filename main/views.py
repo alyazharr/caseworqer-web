dab5eb33647aaa9d9fa6497be4ae2cd945b7614e
Christopher Ekaputra Loe <christopher.ekaputra@ui.ac.id>
2021-10-23 12:28:41 +0700

    Test Corona API - Chris

--- main/views.py
+++ main/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
-# import requests
+import requests
 
 def home(request):
-    # response = requests.get('https://api.kawalcorona.com/indonesia')
-    # coronaData = response.json()
-    # return render(request, 'home.html', {
-    #     'name': coronaData[0]['name'],
-    #     'positif': coronaData[0]['positif'],
-    #     'sembuh': coronaData[0]['sembuh'],
-    #     'meninggal': coronaData[0]['meninggal'],
-    # })
-    return render(request, 'home.html')
+    response = requests.get('https://api.kawalcorona.com/indonesia')
+    coronaData = response.json()
+    return render(request, 'home.html', {
+        'name': coronaData[0]['name'],
+        'positif': coronaData[0]['positif'],
+        'sembuh': coronaData[0]['sembuh'],
+        'meninggal': coronaData[0]['meninggal'],
+    })
+    # return render(request, 'home.html')
