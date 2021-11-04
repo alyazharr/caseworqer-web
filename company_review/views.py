17c180a1a5ddcbe71e90deec982aeee2a82957a5
anantaristik <anantaristik@gmail.com>
2021-11-05 00:47:52 +0700

    company review big update

--- company_review/views.py
+++ company_review/views.py
@@ -1,5 +1,6 @@
 from django.http import response
 from django.shortcuts import render,redirect
+from company_review.models import perusahaanKomen
 from main.models import LowonganKerja
 from company_review.forms import reviewForm
 # Create your views here.
@@ -8,16 +9,34 @@ def cardStar(request):
     username = None
     if request.user.is_authenticated:
         username = request.user.get_username()
-    listJob = LowonganKerja.objects.all()
+    if request.POST and 'search-bar' in request.POST:
+        listJob = LowonganKerja.objects.filter(jobs__icontains = request.POST['search-bar'])
+    else:
+        listJob = LowonganKerja.objects.all()
     response = {'author':username, 'listJob' : listJob}
     return render(request, 'company-review.html', response)
 
-def reviewForms(request):
-    reviewAddForm = reviewForm
-    response = {'addReview':reviewAddForm}
+def read_job(request, id_job):
+    job = LowonganKerja.objects.get(id=id_job)
+    comment = perusahaanKomen.objects.filter(pekerjaan=job)
+    avg = 0
+    for komen in comment:
+        avg+=komen.value
+    avg= int(avg/len(comment))
     if request.method == 'POST':
         post = reviewForm(request.POST)
         if post.is_valid():
-            post.save()
-            return redirect('/company_review')
-        return render(request, 'company_review.html',response)
+            komentar = post.save(job, request.user)
+            print(request.POST)
+            komentar.value = int(request.POST['rate'][0])
+            komentar.save()
+            return redirect('/company_review/')
+    komentar = perusahaanKomen.objects.filter(pekerjaan=job).filter(penulis=request.user)
+    if komentar:
+        sudah_komen = True
+    else:
+        sudah_komen = False
+    return render(request, 'job_company.html', {
+		'job' : job, 'sudah_komen' : sudah_komen, 'comment' : comment,
+        'avg' : avg,
+	})
