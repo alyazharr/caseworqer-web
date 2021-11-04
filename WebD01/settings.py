f03ba5984125ead9827cfb605791b354ba77ee0d
farrelarchini14 <farrelarchini14@gmail.com>
2021-11-04 20:35:46 +0800

    formuser

--- WebD01/settings.py
+++ WebD01/settings.py
@@ -62,6 +62,7 @@ INSTALLED_APPS = [
     'lowongankerja',
     'company_review',
     'tipskarier',
+    'user',
 ]
 
 MIDDLEWARE = [
---
+++ user/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = "user"   
+
+
+urlpatterns = [
+    path("user", views.user_request, name="register")
+]
