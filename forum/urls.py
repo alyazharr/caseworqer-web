9e2deacad16bbf2a1e7a3f93af7f809dc0cb102f
Alya Azhar Agharid <alya.azhar@ui.ac.id>
2021-12-20 02:35:44 +0700

    json azhar(1)

--- forum/urls.py
+++ forum/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('',views.forum, name='forum-post'),
     path('add',views.add_post,name='add-forum'),
     path('<int:forum_pk>',views.forum,name='add-forum-comment'),
+    path('json', views.returnJson, name ='forum-json'),
 ]
--- forum/views.py
+++ forum/views.py
@@ -1,9 +1,15 @@
 from django.http import response
 from django.shortcuts import render,redirect
+from django.http.response import HttpResponse
 from django.views import View
+from django.http import JsonResponse
 from .models import PostForum, PostComment
+from django.core import serializers 
 from .forms import InputForum, CommentSection
+from django.views.decorators.csrf import csrf_exempt
 
+
+data_post = []
 
 def forum(request, forum_pk=None):
     username = "Unknown"
@@ -12,12 +18,13 @@ def forum(request, forum_pk=None):
     PostContent = PostForum.objects.all().values().order_by('-postTime')
     CommentContent = PostComment.objects.all().values()
     comment = CommentSection
+    returnJson()    
     response = {
         'author':username,
         'PostContent': PostContent,
         'CommentContent':CommentContent,
-        'comment':comment
-        }
+        'comment':comment,
+    }
 
     if forum_pk != None:
         content_post = PostForum.objects.get(pk=forum_pk)
@@ -49,3 +56,37 @@ def add_post(request):
             post.save()
         return redirect('/forum')
     return render(request, 'add_forum.html', response)
+
+def returnJson(request):
+    PostContent = PostForum.objects.all().values().order_by('-postTime')
+    CommentContent = PostComment.objects.all().values()
+    flag = False
+    for i in PostContent :
+        for k in data_post:
+            if(i['id'] == k['id']):
+                flag = True
+        if (not flag) :
+            each = {
+                'id': i['id'],
+                'field' : {
+                    'title' : i['title'],
+                    'message' : i['message'],
+                    'time' : i['postTime'],
+                    'user' : i['userPost'],
+                    'comment' : []
+                }
+            }
+            for j in CommentContent :
+                if i['id']==j['post_id']:
+                    tiap = {
+                        'idCom' : j['id'],
+                        'fieldCom': {
+                        'textCom':j['commentText'],
+                        'dateCom':j['commentTime'],
+                        'userCom':j['userCom'] 
+                        }
+                    }
+                    each['field']['comment'].append(tiap)
+            data_post.append(each)
+    result = data_post
+    return JsonResponse(result, safe=False)
