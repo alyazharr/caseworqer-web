5b9244acbde2cc7463c965e142766e652bc9204f
Alya Azhar Agharid <alya.azhar@ui.ac.id>
2021-12-24 00:18:39 +0700

    done forum views

--- forum/views.py
+++ forum/views.py
@@ -10,46 +10,68 @@ from .forms import InputForum, CommentSection
 from django.views.decorators.csrf import csrf_exempt
 import json
 
+@csrf_exempt
 def forum(request, forum_pk=None):
+    response = {}
     username = "Unknown"
-    if request.user.is_authenticated:
-        username = request.user.get_username()
+
     PostContent = PostForum.objects.all().values().order_by('-postTime')
     CommentContent = PostComment.objects.all().values()
-    comment = CommentSection
-    
-    response = {
-        'author':username,
-        'PostContent': PostContent,
-        'CommentContent':CommentContent,
-        'comment':comment,
-    }
 
-    if forum_pk != None:
-        content_post = PostForum.objects.get(pk=forum_pk)
-        if request.method == 'POST':
-            postCom = CommentSection(request.POST)
-            if postCom.is_valid():
-                postCom_new = postCom.save(commit=False)
-                postCom_new.post_id = content_post.id
-                if username != "Unknown": #if user login
-                    postCom_new.userCom = username
-                # or it will replace by the username
-                postCom.save()
-            return redirect('/forum')
-    else:
-        return render(request, 'forum.html', response)
+    try :
+        get_json_com = json.loads(request.body)
+        print(get_json_com)
+        inputCom = CommentSection(get_json_com)
+        # mengaitkan comment dengan parent dari data yang sudah dikirim json
+        if inputCom.is_valid():
+            postCom_new = inputCom.save(commit=False)
+            postCom_new.post_id = get_json_com['post']
+        inputCom.save()
+        web = False # jika berasal dari mobile
+
+    except :
+        web = True
+
+        # pada web terdapat autentikasi
+        if request.user.is_authenticated:
+            username = request.user.get_username()
+        
+        inputCom = CommentSection(request.POST or None)
+        
+        response['author'] = username
+        response['PostContent'] = PostContent
+        response['CommentContent'] = CommentContent
+        response['comment'] = inputCom
+
+    if inputCom.is_valid() and request.method == 'POST' and web==True:
+        if forum_pk != None:
+            content_post = PostForum.objects.get(pk=forum_pk)
+            if request.method == 'POST':
+                inputCom = CommentSection(request.POST)
+                if inputCom.is_valid():
+                    postCom_new = inputCom.save(commit=False)
+                    postCom_new.post_id = content_post.id
+                    if username != "Unknown": #if user login
+                        postCom_new.userCom = username
+                    # or it will replace by the username
+                    inputCom.save()
+                return redirect('/forum')
+        else : 
+            return render(request, 'forum.html', response)
+
+    response['comment'] = inputCom
+    return render(request, 'forum.html', response)  
 
 @csrf_exempt
 def add_post(request):
-    username = "Unknown"
     response = {}
     try :
         get_json = json.loads(request.body)
         print(get_json)
         inputPost = InputForum(get_json)
-        post_new = inputPost.save(commit=False)
-        post_new.userPost = username
+        if inputPost.is_valid():
+            postCom_new = inputPost.save(commit=False)
+            postCom_new.userPost = get_json['userPost']
         inputPost.save()
         web = False
     except :
@@ -67,6 +89,8 @@ def add_post(request):
     response['inputContent'] = inputPost
     return render(request, 'add_forum.html', response)
 
+
+# method pengiriman data json
 def post_json(request):
     data_post = serializers.serialize('json', PostForum.objects.all())
     return HttpResponse(data_post, content_type="application/json")
