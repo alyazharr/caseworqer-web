524caf0ff0ab5cea2e6ded194be9080e05c1ef4e
Nafhan Raissa Syandana <nafhan.raissa@ui.ac.id>
2021-12-24 08:26:52 +0700

    json

--- lowongankerja/urls.py
+++ lowongankerja/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .views import jsonMethod
 
 app_name = 'lowongankerja'
 urlpatterns = [
--- lowongankerja/views.py
+++ lowongankerja/views.py
@@ -1,7 +1,11 @@
 from django.http.response import HttpResponseRedirect
+from django.http.response import HttpResponse
 from django.shortcuts import render, redirect
 from main.models import LowonganKerja
 from .forms import PembukaLowonganForm
+from django.core import serializers
+from django.views.decorators.csrf import csrf_exempt
+import json
 import requests
 
 # Create your views here.
@@ -10,15 +14,36 @@ import requests
 #    response = {'lowongan': lowongan}
 #    return render(request, 'indexlowongan.html', response)
 
+@csrf_exempt
 def lowonganForm(request):
-    if request.method == 'POST':
+    context = {}
+    # object form
+    try:
+        recJson = json.loads(request.body)
+        print(recJson)
+        form = PembukaLowonganForm(recJson)
+        form.save()
+        web = False
+    except:
+        form = PembukaLowonganForm(request.POST or None)
+        web = True
+
+    if request.method == 'POST' and web == True:
         form = PembukaLowonganForm(request.POST)
         if form.is_valid():
-            print("berhasil")
+            #print("berhasil")
             form.save()
             return redirect('../lowongankerja/lowonganForm')
         
     else:
         form = PembukaLowonganForm()
 
+    system = request.POST.get('system', None)
+    context['form'] = form
+    context['system'] = system
+
     return render(request, 'bukaLowongan.html', {'form': form})
+
+def jsonMethod(request):
+    data = serializers.serialize('json', LowonganKerja.objects.all())
+    return HttpResponse(data, content_type="application/json")
