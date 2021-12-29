6aea9b72c77a1e06028e72954700ab05c0e96f1b
anantaristik <anantaristik@gmail.com>
2021-12-30 03:19:46 +0700

    nambah views

--- company_review/views.py
+++ company_review/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.decorators import login_required
 from django.http.response import JsonResponse,HttpResponse
 from django.core import serializers 
 from django.views.decorators.csrf import csrf_exempt
-import json
+import json as djson
 # Create your views here.
 
 def cardStar(request):
@@ -35,30 +35,30 @@ def read_job(request, id_job):
     else:
         avg = (avg/len(comment))
 
-    try:
-        get_json = json.loads(request.body)
-        print(get_json)
-        addReview = reviewForm(get_json)
+    # try:
+    #     get_json = json.loads(request.body)
+    #     print(get_json)
+    #     addReview = reviewForm(get_json)
 
-        if addReview.is_valid():
-            addReview_new = addReview.save(commit=False)
-            addReview_new.pekerjaan_id = get_json['pekerjaan']
-        addReview.save()
-        web = False
+    #     if addReview.is_valid():
+    #         addReview_new = addReview.save(commit=False)
+    #         addReview_new.pekerjaan_id = get_json['pekerjaan']
+    #     addReview.save()
+    #     web = False
     
-    except:
-        web = True
-        if request.user.is_authenticated:
-            penulis  = request.user.get_username()
+    # except:
+    #     web = True
+    #     if request.user.is_authenticated:
+    #         penulis  = request.user.get_username()
         
-        addReview = reviewForm(request.POST or None)
-        value = addReview.save(job, request.user)
-        value.value = int(request.POST['rate'][0])
+    #     addReview = reviewForm(request.POST or None)
+    #     value = addReview.save(job, request.user)
+    #     value.value = int(request.POST['rate'][0])
 
-        response['pekerjaan'] = job
-        response['penulis'] = penulis
-        response['value'] = value.value
-        response['description'] = addReview
+    #     response['pekerjaan'] = job
+    #     response['penulis'] = penulis
+    #     response['value'] = value.value
+    #     response['description'] = addReview
         
     if request.method == 'POST' and request.is_ajax and request.user.is_authenticated and web==True:
         post = reviewForm(request.POST)
@@ -80,6 +80,34 @@ def read_job(request, id_job):
 		'job' : job, 'sudah_komen' : sudah_komen, 'comment' : comment,
         'avg' : avg,
 	})
+
+@csrf_exempt
+def postMethod(request, id_job):
+    # taken and edited from https://stackoverflow.com/questions/40059654/python-convert-a-bytes-array-into-json-format/40060181
+    if (request.method == 'POST'):
+        data = djson.loads(request.body)
+        jobs = data['pekerjaan']
+        penulis = data['penulis']
+        value = data['value']
+        description = data['description']
+
+        job = LowonganKerja.objects.get(id=id_job)
+        data_baru = perusahaanKomen.objects.filter(pekerjaan=job)
+        target = None
+        for i in data_baru:
+            if i == perusahaanKomen.objects.filter(pekerjaan=job):
+                target = i
+                break
+        
+        target.jobs = jobs
+        target.penulis = penulis
+        target.value = value
+        target.description = description
+
+        target.save()
+
+        return ({'status':200})
+
 
 def json_joblist(request):
     data_joblist = serializers.serialize('json', LowonganKerja.objects.all())
